caec6c81b48a4238a3ae3a0a57443ecbd6b46d2c
NSGuliyev <nesimi018@yahoo.com>
2020-02-16 17:32:34 -0500

    Burger app is ready

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 // Set up MySQL connection.
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Nn18101988',
-    database: 'burgers_db'
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Nn18101988",
+  database: "burgers_db"
 });
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.log('Error connectiong: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-// Export conection for our ORM to use.
+// Export connection for our ORM to use.
 module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers ( burger_name) VALUES ('BEAUVINE');
+INSERT INTO burgers ( burger_name) VALUES ('BELLYTIMBER');
+INSERT INTO burgers ( burger_name, devoured) VALUES ('JOE BEEF', true);
+INSERT INTO burgers ( burger_name, devoured) VALUES ('LE BENNY', true);
+INSERT INTO burgers ( burger_name, devoured) VALUES ('BELLE RINGER', true);
+INSERT INTO burgers ( burger_name) VALUES ('LAFAYETTE');
